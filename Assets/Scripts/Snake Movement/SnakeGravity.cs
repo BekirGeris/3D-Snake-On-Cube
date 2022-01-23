14cd68a2eecb93558854334fa14380fe699fdca7
bekir geriş <bekir.geris@gmail.com>
2022-01-23 20:23:37 +0300

    yılan hareketinde iyileştirmeler yapıldı

--- Assets/Scripts/Snake Movement/SnakeGravity.cs
+++ Assets/Scripts/Snake Movement/SnakeGravity.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SnakeGravity : MonoBehaviour
 {
-    const float G = 10000f;
+    const float G = 1000f;
 
     private List<Transform> targetObjects = new List<Transform>();
     private Cube[] cubes;
--- Assets/Scripts/Snake Movement/SnakeMoveController.cs
+++ Assets/Scripts/Snake Movement/SnakeMoveController.cs
@@ -12,6 +12,10 @@ public class SnakeMoveController : MonoBehaviour
     private List<GameObject> bodyParts = new List<GameObject>();
     private List<Vector3> positionHistory = new List<Vector3>();
 
+    private Vector3 localScale;
+    private int tailLength;
+    private float snakeLength;
+
     public float moveSpeed = 5;
     public float steerSpeed = 90;
     public int gap = Mathf.Abs(10);
@@ -19,12 +23,12 @@ public class SnakeMoveController : MonoBehaviour
 
     public bool rigtFlag = false;
     public bool leftFlag = false;
-    public bool grow = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0;i<= 100; i++)
+        localScale = BodyPrefab.transform.localScale;
+        for (int i = 0;i<= 200; i++)
         {
             growSnake();
         }
@@ -37,42 +41,70 @@ public class SnakeMoveController : MonoBehaviour
         {
             transform.position += transform.forward * moveSpeed * Time.deltaTime;
 
-            if (rigtFlag)
-            {
-                right();
-            }
+            orientationSnake();
 
-            if (leftFlag)
-            {
-                left();
-            }
+            storePositionHistory();
 
-            //store position history
-            positionHistory.Insert(0, transform.position);
+            moveBodyParts();
+        }
+    }
 
-            if(positionHistory.Count == 5000)
-            {
-                positionHistory.RemoveAt(positionHistory.Count - 1);
-            }
-            Debug.Log(positionHistory.Count);
-
-            //move body parts
-            if(positionHistory.Count > fark)
-            {
-                int index = fark;
-                foreach (var body in bodyParts)
-                {
-                    Vector3 point = positionHistory[Mathf.Min(index * gap, positionHistory.Count - 1)];
-                    body.transform.position = point;
-                    index++;
-                }
-            }
+    private void orientationSnake()
+    {
+        if (rigtFlag)
+        {
+            right();
         }
 
-        if (grow)
+        if (leftFlag)
         {
-            growSnake();
-            grow = false;
+            left();
+        }
+    }
+
+    private void storePositionHistory()
+    {
+        //store position history
+        positionHistory.Insert(0, transform.position);
+
+        if (positionHistory.Count == 5000)
+        {
+            positionHistory.RemoveAt(positionHistory.Count - 1);
+        }
+    }
+    private void moveBodyParts()
+    {
+        //move body parts
+        if (positionHistory.Count > fark)
+        {
+            int index = fark;
+            foreach (var body in bodyParts)
+            {
+                Vector3 point = positionHistory[Mathf.Min(index * gap, positionHistory.Count - 1)];
+                body.transform.position = point;
+                if(index >= snakeLength - tailLength)
+                {
+                    if(index >= snakeLength - tailLength / 2)
+                    {
+                        if (index >= snakeLength - tailLength / 4)
+                        {
+                            //yýlanýn uzunluðunun 1/8'ü son kýsým
+                            body.transform.localScale = localScale * (snakeLength / (snakeLength / 2 + index + 80));
+                        }
+                        else
+                        {
+                            //yýlanýn uzunluðunun 1/8'ü
+                            body.transform.localScale = localScale * (snakeLength / (snakeLength / 2 + index + 40));
+                        }
+                    }
+                    else
+                    {
+                        //yýlanýn uzunluðunun 1/4'ü
+                        body.transform.localScale = localScale * (snakeLength / (snakeLength / 2 + index + 20));
+                    }
+                }
+                index++;
+            }
         }
     }
 
@@ -88,6 +120,8 @@ public class SnakeMoveController : MonoBehaviour
             body = Instantiate(BodyPrefab);
         }
         bodyParts.Add(body);
+        tailLength = bodyParts.Count / 2;
+        snakeLength = bodyParts.Count;
     }
 
     public void right()
